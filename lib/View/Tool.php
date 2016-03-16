5aff9bcf1e1981a055a9dcc99266d6451b896e81
Gowrav Vishwakarma <gowravvishwakarma@gmail.com>
2016-03-16 16:38:01 +0530

    Moved Tool Helper from base to cms

--- lib/View/Tool.php
+++ lib/View/Tool.php
@@ -12,12 +12,12 @@ class View_Tool extends \View{
 
 	function setModel($model,$fields=null){
 		$m = parent::setModel($model,$fields);
-		$this->add('xepan\base\Controller_Tool_Optionhelper');
+		$this->add('xepan\cms\Controller_Tool_Optionhelper');
 		return $m;
 	}
 
 	function render(){
-		if($this->app->isEditing){
+		if(@$this->app->isEditing){
 			$this->js(true)
 				->_load($this->api->url()->absolute()->getBaseURL().'vendor/xepan/cms/templates/js/xepanComponent.js')
 				->xepanComponent($this->option_panel_options);
