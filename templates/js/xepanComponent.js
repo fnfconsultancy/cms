41e26c5b5eb3eff3c88cd20a7d171ecd141eac41
Gowrav Vishwakarma <gowravvishwakarma@gmail.com>
2016-03-16 18:00:07 +0530

    xepanComponent js now called up in front double click

--- templates/js/xepanComponent.js
+++ templates/js/xepanComponent.js
@@ -1,8 +1,14 @@
 jQuery.widget("ui.xepanComponent",{
 	
 	_create: function(){
-		// $(this.element).css('background-color','red');
-		console.log(this.options);
+		this.createOptionPanel();
+		$(this.element).on('dblclick',function(elm){
+			alert("hello");
+		})
+	},
+
+	createOptionPanel: function(){
+		
 	}
 	
 });
