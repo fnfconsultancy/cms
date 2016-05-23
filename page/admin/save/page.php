796bb66aaa964a0948f5ae3d77413fd17c6392d9
Gowrav Vishwakarma <gowravvishwakarma@gmail.com>
2016-05-23 19:25:56 +0530

    send proper error if cannot save page

--- page/admin/save/page.php
+++ page/admin/save/page.php
@@ -21,24 +21,28 @@ class page_admin_save_page extends \Page {
 		}
 		
 		if ( $_POST['length'] != strlen( $_POST['body_html'] ) ) {
-			$this->js()->univ()->successMessage( 'Length send ' . $_POST['length'] . " AND Length calculated again is " . strlen( $_POST['body_html'] ) )->execute();
+			$this->js()->univ()->errorMessage( 'Length send ' . $_POST['length'] . " AND Length calculated again is " . strlen( $_POST['body_html'] ) )->execute();
 		}
 
 		if ( $_POST['crc32'] != sprintf("%u",crc32( $_POST['body_html'] ) )) {
-			$this->js()->univ()->successMessage( 'CRC send ' . $_POST['crc32'] . " AND CRC calculated again is " . sprintf("%u",crc32( $_POST['body_html'] )) )->execute();
+			$this->js()->univ()->errorsMessage( 'CRC send ' . $_POST['crc32'] . " AND CRC calculated again is " . sprintf("%u",crc32( $_POST['body_html'] )) )->execute();
 		}
 
 		if(strpos($_POST['file_path'], realpath('websites/'.$this->app->current_website_name)!==0)){
 			$this->js()->univ()->errorMessage('You cannot save in this location')->execute();
 		}
+
 		$html_content = urldecode( trim( $_POST['body_html'] ) );
 
 		// convert all absolute url to relative
 		$domain = $this->app->pm->base_url.$this->app->pm->base_path.'websites/'.$this->app->current_website_name.'/www/';
 		$html_content = str_replace($domain, '', $html_content);
+		try{
+			file_put_contents($_POST['file_path'], $html_content);
+			$this->js()->_selectorDocument()->univ()->successMessage("Content Saved")->execute();
+		}catch(\Exception $e){
+			$this->js()->_selectorDocument()->univ()->errorMessage($e->getMessage())->execute();
+		}
 
-		file_put_contents($_POST['file_path'], $html_content);
-
-		$this->js()->_selectorDocument()->univ()->successMessage("Content Saved")->execute();
 	}
 }
