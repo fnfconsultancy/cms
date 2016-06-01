94f0de88738b187d8c84ddee593753557648caea
vijay mali <guddavijay@gmail.com>
2016-06-01 17:06:18 +0530

    css option view created & colorpicker js & css include

---
+++ lib/View/CssOptions.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace xepan\cms;
+
+class View_CssOptions extends \View{
+	function init(){
+		parent::init();
+	}
+
+	function render(){
+		$this->api->jquery->addStaticStyleSheet('colorpicker/pick-a-color-1.1.8.min');
+		$this->js()
+			->_load('colorpicker/tinycolor-0.9.15.min')
+			->_load('colorpicker/pick-a-color-1.1.8.min')
+			->_load('colorpicker/colorpicker');
+			;
+		$this->js(true)->_selector('.epan-color-picker')->univ()->xEpanColorPicker();
+		parent::render();
+	}
+
+	function defaultTemplate(){
+		return ['view/cms/toolbar/basic'];
+	}
+}
