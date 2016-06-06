cd1f963479bdd7f9e1c2aee377a6fd75fa1eba70
vijay mali <guddavijay@gmail.com>
2016-06-06 15:16:42 +0530

    elfinder text editor with codemirror working

--- page/websites.php
+++ page/websites.php
@@ -10,17 +10,18 @@ class page_websites extends \xepan\base\Page{
 		// as per page 
 		// http://codepen.io/kaizoku-kuma/pen/JDxtC
 		$this->app->jui->addStylesheet('codemirror/codemirror-5.15.2/lib/codemirror');
-		$this->app->jui->addStylesheet('codemirror/codemirror-5.15.2/theme/ambiance');
-		$this->app->jui->addStylesheet('codemirror/codemirror-5.15.2/theme/dracula');
+		$this->app->jui->addStylesheet('codemirror/codemirror-5.15.2/theme/solarized');
 
 		$this->app->jui->addStaticInclude('codemirror/codemirror-5.15.2/lib/codemirror');
 		$this->app->jui->addStaticInclude('codemirror/codemirror-5.15.2/mode/htmlmixed/htmlmixed');
 		$this->app->jui->addStaticInclude('codemirror/codemirror-5.15.2/mode/jade/jade');
 		$this->app->jui->addStaticInclude('codemirror/codemirror-5.15.2/mode/php/php');
 		$this->app->jui->addStaticInclude('codemirror/codemirror-5.15.2/mode/xml/xml');
+		$this->app->jui->addStaticInclude('codemirror/codemirror-5.15.2/mode/css/css');
+		$this->app->jui->addStaticInclude('codemirror/codemirror-5.15.2/mode/javascript/javascript');
 		// $this->app->jui->addStaticInclude('codemirror/codemirror-5.15.2/keymap/sublime');
 		// $this->app->jui->addStaticInclude('codemirror/codemirror-5.15.2/keymap/emacs');
-		// $this->js()->_load('codemirror/codemirror-5.15.2/lib/codemirror');
+		// $this->app->jui->addStaticInclude('codemirror/codemirror-5.15.2/keymap/vim');
 		
 		$this->js(true,'
 				$("#'.$this->name.'").elfinder({
@@ -30,10 +31,17 @@ class page_websites extends \xepan\base\Page{
 							// list of allowed mimetypes to edit // if empty - any text files can be edited mimes : [],
 							// you can have a different editor for different mimes 
 							editors : [{
-								addKeyMap:["emacs","top"],
 								mimes : ["text/plain", "text/html","text/x-jade", "text/javascript", "text/css", "text/x-php", "application/x-httpd-php", "text/x-markdown", "text/plain", "text/html", "text/javascript", "text/css"],
 								load : function(textarea) {
-									this.myCodeMirror = CodeMirror.fromTextArea(textarea, { lineNumbers: true, theme: "dracula", viewportMargin: Infinity, lineWrapping: true, htmlMode: true});
+									this.myCodeMirror = CodeMirror.fromTextArea(textarea, { 
+																					lineNumbers: true,
+																					theme: "solarized",
+																					viewportMargin: Infinity, 
+																					lineWrapping: true, 
+																					mode:"javascript",json:true,
+																					mode:"css",css:true , 
+																					htmlMode: true
+																				});
 								},
 								close : function(textarea, instance) { 
 									this.myCodeMirror = null; 
