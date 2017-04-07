5b9147491e36692291c58399bbd577b5f175a811
sinharakesh <rksinha.btech@gmail.com>
2017-04-07 18:00:16 +0530

    new colorpicker added

--- templates/js/colorpicker/colorpicker.js
+++ templates/js/colorpicker/colorpicker.js
@@ -1,5 +1,19 @@
 $.each({
 	xEpanColorPicker: function(){
-		$(this.jquery).pickAColor();
+		$(this.jquery).colorpicker({
+			parts:'full',
+	        alpha:false,
+	        showOn:'both',
+	        buttonColorize:true,
+	        showNoneButton:true,
+	        position: {
+				my: 'center',
+				at: 'center',
+				of: window
+			},
+			modal: true,
+			buttonImage:'vendor/xepan/cms/templates/css/colorpicker/images/ui-colorpicker.png'
+		});
+		// $(this.jquery).pickAColor();
 	}
 },$.univ._import);
