d49b762ac0d1ffeda588e6a6ab3961fbdfcfcf64
sinharakesh <rksinha.btech@gmail.com>
2017-04-08 16:37:30 +0530

    working on option of bootstrap menu bar

--- lib/Tool/BootStrapMenu.php
+++ lib/Tool/BootStrapMenu.php
@@ -2,19 +2,15 @@
 
 namespace xepan\cms;
 
-/**
-* 
-*/
 class Tool_BootStrapMenu extends \xepan\cms\View_Tool{
-	public $options=[
-						'show_brand'=>true,
-						'#nav-position'=>'relative'
-	];
+	public $runatServer = false;
+	public $teplateOverridable=true;
+	
 	function init(){
 		parent::init();
 
 	}
-	
+
 	function defaultTemplate(){
 		return ['xepan/tool/bootstrap-menu'];
 	}
