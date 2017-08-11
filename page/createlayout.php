5e00a02d613c6b99b87a2a53ecd13856407de923
sinharakesh <rksinha.btech@gmail.com>
2017-08-11 10:50:01 +0530

    layout creation in custom template or theme template

--- page/createlayout.php
+++ page/createlayout.php
@@ -15,10 +15,18 @@ class page_createlayout extends \Page{
         $sub_domain = str_replace('www.','',$this->app->extract_subdomains($url))?:'www';
 		
 		$base_path	= $this->app->pathfinder->base_location->base_path.'/websites/'.$this->app->current_website_name."/www";
+		
 		// check websitelayout folder is exist or not
-		$folder_path = $base_path."/themelayout";
+		if($this->app->epan['is_template']){
+			$folder_name = 'themelayout';
+		}else{
+			$folder_name = 'customlayout';
+		}
+
+		// for template layout
+		$folder_path = $base_path."/".$folder_name;
 		if(!file_exists(realpath($folder_path))){
-			\Nette\Utils\FileSystem::createDir('./websites/'.$this->app->current_website_name.'/www/themelayout');
+			\Nette\Utils\FileSystem::createDir('./websites/'.$this->app->current_website_name.'/www/'.$folder_name);
 		}
 
 		//check file with name is already exist
@@ -30,7 +38,7 @@ class page_createlayout extends \Page{
 			exit;
 		}
 
-		$fs = \Nette\Utils\FileSystem::write('./websites/'.$this->app->current_website_name.'/www/themelayout/'.$file_name,$file_content);
+		$fs = \Nette\Utils\FileSystem::write('./websites/'.$this->app->current_website_name.'/www/'.$folder_name.'/'.$file_name,$file_content);
 
 		echo json_encode($return);
 		exit;
