7d616c059ab0ee7295bbd707532f7efaf2a589f1
sinharakesh <rksinha.btech@gmail.com>
2018-05-29 13:24:56 +0530

    owl slider are added

--- lib/Tool/Testimonial.php
+++ lib/Tool/Testimonial.php
@@ -10,13 +10,25 @@ class Tool_Testimonial extends \xepan\cms\View_Tool{
 		'tittle_show'=>true,
 		'describtion_show'=>true,
 		'rating_show'=>true,
-		'testimonial_row'=>4
+		'items'=>3,
+		'margin_between_slide'=>10,
+		'loop'=>true,
+		'startPosition'=>1,
+		'nav'=>true,
+		'slideBy'=>1,
+		'autoplay'=>false
 	];
 
 	public $cat_model;
+	public $lister;
+
 	function init(){
 		parent::init();
 		
+		$this->js(true)->_css('../owlslider/assets/owl.carousel');
+		$this->js(true)->_css('../owlslider/assets/owl.theme.default');
+		$this->js()->_load('../owlslider/owl.carousel.min');
+
 		if($this->owner instanceof \AbstractController){
 			$this->add('View')->set('I am Testimonial Tool')->addClass('alert alert-info');
 			return;		
@@ -29,11 +41,6 @@ class Tool_Testimonial extends \xepan\cms\View_Tool{
 		if($this->options['category_show']){
 
 		}
-		
-		// $this->js(true)->_css('jquery.easing.1.3');
-		// $this->js(true)->_css('fancybox/jquery.fancybox');
-		// $this->app->jquery->addStaticInclude('slidepro/jquery.sliderPro.min');
-		// $this->app->jquery->addStaticInclude('fancybox/jquery.fancybox.pack');
 
 		$this->cat_model = $cat_model = $this->add('xepan\cms\Model_TestimonialCategory');
 		$cat_model->addCondition('status','Active');
@@ -49,10 +56,40 @@ class Tool_Testimonial extends \xepan\cms\View_Tool{
 		$testimonial_model->addCondition('status','Aprroved');
 		$testimonial_model->addCondition('category_id',$cat_model->id);
 		
-		$lister = $this->add('CompleteLister',null,null,['view/tool/cms/testimoniallist']);
+		$this->lister = $lister = $this->add('CompleteLister',null,null,['view/tool/cms/testimoniallist']);
 		$lister->setModel($testimonial_model);
-	
 		
+	}
 
+	function render(){
+		$owl_options = [
+				'items'=>$this->options['items'],
+				'margin'=>$this->options['margin_between_slide'],
+				'loop'=>$this->options['loop'],
+				'startPosition'=>$this->options['startPosition'],
+				'nav'=>$this->options['nav'],
+				'slideBy'=>$this->options['slideBy'],
+				'autoplay'=>$this->options['autoplay'],
+				'lazyLoad'=>true,
+				'responsiveClass'=>true,
+				'responsive'=>[
+					        0=>[
+					            'items'=>1,
+					            'nav'=>true
+					        ],
+					        600=>[
+					            'items'=>3,
+					            'nav'=>true
+					        ],
+					        1000=>[
+					            'items'=>5,
+					            'nav'=>true,
+					            'loop'=>false
+					        ]
+					    ],
+			];
+
+		$this->js(true)->_selector('.owl-carousel')->owlCarousel($owl_options);
+		parent::render();
 	}
 }
