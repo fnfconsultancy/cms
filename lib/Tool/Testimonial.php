6cf3b71442d32840e1dfb966e133d490c0d781d5
nirmalp1994 <nirmalp794@gmail.com>
2018-06-11 06:09:08 -0700

    layouts and options

--- lib/Tool/Testimonial.php
+++ lib/Tool/Testimonial.php
@@ -6,17 +6,19 @@ class Tool_Testimonial extends \xepan\cms\View_Tool{
 		'testimonial_category'=>1,
 		'allow_add'=>false,
 		'category_show'=>false,
-		'image_show'=>true,
-		'tittle_show'=>true,
-		'describtion_show'=>true,
-		'rating_show'=>true,
-		'items'=>3,
+		'show_image'=>true,
+		'show_title'=>true,
+		'show_description'=>true,
+		'show_rating'=>true,
 		'margin_between_slide'=>10,
 		'loop'=>true,
+		'items'=>2,
 		'startPosition'=>1,
 		'nav'=>true,
-		'slideBy'=>1,
-		'autoplay'=>false
+		'slideBy'=>2,
+		'autoplay'=>true,
+		'layout'=>'testimoniallist',
+		'custom_template'=>null
 	];
 
 	public $cat_model;
@@ -24,7 +26,6 @@ class Tool_Testimonial extends \xepan\cms\View_Tool{
 
 	function init(){
 		parent::init();
-		
 		$this->js(true)->_css('../owlslider/assets/owl.carousel');
 		$this->js(true)->_css('../owlslider/assets/owl.theme.default');
 		$this->js()->_load('../owlslider/owl.carousel.min');
@@ -35,10 +36,11 @@ class Tool_Testimonial extends \xepan\cms\View_Tool{
 		} 
 		
 		if(!$this->options['testimonial_category']){
-			$this->add('View')->set('Please select category first form it\'s options')->addClass('alert alert-danger');
+		$this->add('View')->set('Please select category first form it\'s options')->addClass('alert alert-danger');
 			return;
 		}
 		if($this->options['category_show']){
+
 
 		}
 
@@ -56,10 +58,46 @@ class Tool_Testimonial extends \xepan\cms\View_Tool{
 		$testimonial_model->addCondition('status','Aprroved');
 		$testimonial_model->addCondition('category_id',$cat_model->id);
 		
-		$this->lister = $lister = $this->add('CompleteLister',null,null,['view/tool/cms/testimoniallist']);
+		$layout = $this->options['layout'];
+		if($this->options['custom_template']){
+			$layout = $this->options['custom_template'];
+			$path = getcwd()."/websites/".$this->app->current_website_name."/www/view/tool/testimonial/".$layout.".html";
+			if(!file_exists($path)){
+				$this->add('View_Warning')->set('your define template('.$layout.') not found at location /www/view/tool/testimonial/'.$layout.'.html');
+				$layout = $this->options['layout'];
+				return;
+			}
+		} 
+
+		$this->lister = $lister = $this->add('CompleteLister',null,null,['view/tool/cms/testimonial/'.$layout]);
 		$lister->setModel($testimonial_model);
-		
+		$lister->add('xepan\cms\Controller_Tool_Optionhelper',['options'=>$this->options,'model'=>$testimonial_model]);
 	}
+	
+	function addToolCondition_row_show_title($value,$l){
+		if($value) return;
+		$l->template->tryDel('name_wrapper');
+	}
+
+	function addToolCondition_row_show_describtion($value,$l){
+		if($value) return;
+
+		$l->template->tryDel('description_wrapper');
+	}
+		function addToolCondition_row_show_image($value,$l){
+		if($value) return;
+
+		$l->template->tryDel('image_wrapper');
+	}
+
+	function addToolCondition_row_show_rating($value,$l){
+		if($value) return;
+
+		$l->template->tryDel('rating_wrapper');
+	}
+
+
+
 
 	function render(){
 		$owl_options = [
@@ -69,24 +107,9 @@ class Tool_Testimonial extends \xepan\cms\View_Tool{
 				'startPosition'=>$this->options['startPosition'],
 				'nav'=>$this->options['nav'],
 				'slideBy'=>$this->options['slideBy'],
-				'autoplay'=>$this->options['autoplay'],
+				'autoplay'=>true,
 				'lazyLoad'=>true,
 				'responsiveClass'=>true,
-				'responsive'=>[
-					        0=>[
-					            'items'=>1,
-					            'nav'=>true
-					        ],
-					        600=>[
-					            'items'=>3,
-					            'nav'=>true
-					        ],
-					        1000=>[
-					            'items'=>5,
-					            'nav'=>true,
-					            'loop'=>false
-					        ]
-					    ],
 			];
 
 		$this->js(true)->_selector('.owl-carousel')->owlCarousel($owl_options);
