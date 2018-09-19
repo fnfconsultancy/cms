fe09deb5113065e9191ae9404d6841dc5f353bf4
sinharakesh <rksinha.btech@gmail.com>
2018-09-19 18:48:41 +0530

    testimonial tool layout correction

--- lib/Tool/Testimonial.php
+++ lib/Tool/Testimonial.php
@@ -4,15 +4,14 @@ namespace xepan\cms;
 class Tool_Testimonial extends \xepan\cms\View_Tool{
 	public $options = [
 		'testimonial_category'=>0,
-		'allow_add'=>false,
-		'category_show'=>false,
+		'show_category_name'=>false,
 		'show_image'=>true,
 		'show_title'=>true,
 		'show_description'=>true,
 		'show_rating'=>true,
 		'margin_between_slide'=>10,
-		'loop'=>true,
-		'display_items'=>3,
+		'loop'=>false,
+		'display_items'=>2,
 		'startPosition'=>1,
 		'nav'=>true,
 		'slideBy'=>2,
@@ -26,7 +25,7 @@ class Tool_Testimonial extends \xepan\cms\View_Tool{
 
 	function init(){
 		parent::init();
-		$this->js(true)->_css('../owlslider/assets/owl.carousel');
+		// $this->js(true)->_css('../owlslider/assets/owl.carousel');
 		$this->js(true)->_css('../owlslider/assets/owl.theme.default');
 		$this->js()->_load('../owlslider/owl.carousel.min');
 
@@ -67,57 +66,63 @@ class Tool_Testimonial extends \xepan\cms\View_Tool{
 		$this->lister = $lister = $this->add('CompleteLister',null,null,['view/tool/cms/testimonial/'.$layout]);
 		$lister->setModel($testimonial_model);
 		$lister->add('xepan\cms\Controller_Tool_Optionhelper',['options'=>$this->options,'model'=>$testimonial_model]);
+		
+		$this->lister->template->trySet('display_items',$this->options['display_items']);
 	}
 	
 	function addToolCondition_row_show_title($value,$l){
-		if($value) return;
-		$l->template->tryDel('name_wrapper');
+		if(!$value) $l->template->tryDel('name_wrapper');
 	}
 
-	function addToolCondition_row_show_describtion($value,$l){
-		if($value) return;
+	function addToolCondition_row_show_category_name($value,$l){
+		if(!$value){
+			$l->template->tryDel('category_wrapper');
+			$l->template->tryDel('category');
+		} 
+	}
 
-		$l->template->tryDel('description_wrapper');
+	function addToolCondition_row_show_description($value,$l){
+		if(!$value) $l->template->tryDel('description_wrapper');
 	}
 
 	function addToolCondition_row_show_image($value,$l){
-		if($value) return;
-
-		$l->template->tryDel('image_wrapper');
+		if(!$value) $l->template->tryDel('image_wrapper');
 	}
 
 	function addToolCondition_row_show_rating($value,$l){
 		if(!$value){
 			$l->template->tryDel('rating_wrapper');
 			return;
-		} 
-		// $form = $l->add('Form',null,'rating');
-		// $rating_field = $form->addField('xepan\base\Rating','rating','')
-		// 	->setValueList(['1'=>'1','2'=>'2','3'=>'3','4'=>'4','5'=>'5'])->set($l->model['rating']);
-		// $rating_field->initialRating = $l->model['rating'];
-		// $rating_field->readonly = true;
-		// $l->current_row_html['rating'] = $form->getHtml();
+		}
 
+		$form = $l->add('Form',['name' => "rating_view_".$l->model->id],'rating');
+		$rating_field = $form->addField('xepan\base\Rating','rating','')
+			->setValueList(['1'=>'1','2'=>'2','3'=>'3','4'=>'4','5'=>'5'])->set($l->model['rating']);
+		$rating_field->initialRating = $l->model['rating'];
+		$rating_field->readonly = true;
+		$l->current_row_html['rating'] = $form->getHtml();
 	}
 
 	function render(){
 
 		$owl_options = [
-				'items'=>'1'
-				// 'items'=>$this->options['display_items'],
-				// 'margin'=>$this->options['margin_between_slide'],
-				// 'loop'=>$this->options['loop'],
-				// 'startPosition'=>$this->options['startPosition'],
-				// 'nav'=>$this->options['nav'],
-				// 'slideBy'=>$this->options['slideBy'],
-				// 'autoplay'=>true,
-				// 'lazyLoad'=>true,
-				// 'responsiveClass'=>true,
+				'items'=>1,
+				'loop'=>false,
+				'items'=>$this->options['display_items'],
+				'margin'=>$this->options['margin_between_slide'],
+				'loop'=>$this->options['loop'],
+				'startPosition'=>$this->options['startPosition'],
+				'nav'=>$this->options['nav'],
+				'slideBy'=>$this->options['slideBy'],
+				'autoplay'=>true,
+				'lazyLoad'=>true,
+				'responsiveClass'=>true,
+				'responsive'=>[
+						10=>['items'=>1],
+						1000=>['items'=>1]
+					]
 			];
-
-		// $this->app->print_r($this->options);
-		// $this->app->print_r($owl_options,true);
-		$this->js(true)->_selector('.slider-wrapper-'.$this->lister->name)->owlCarousel();
+		// $this->js()->_selector('.slider-wrapper-'.$this->lister->name)->owlCarousel($owlopt);
 		parent::render();
 	}
 }
